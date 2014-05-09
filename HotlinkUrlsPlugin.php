a13692d4a643726b294a627acd73f99508dd0fa3
Michael Slone <m.slone@gmail.com>
2014-05-09 13:33:59 -0400

    initial commit

---
+++ HotlinkUrlsPlugin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * HotlinkUrls
+ * 
+ * @copyright Copyright 2014 Michael Slone 
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ */
+
+/**
+ * The HotlinkUrls plugin.
+ * 
+ * @package Omeka\Plugins\HotlinkUrls
+ */
+class HotlinkUrlsPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    protected $_filters = array(
+        'hotlinkIdentifier' => array('Display', 'Item', 'Dublin Core', 'Identifier'),
+        'hotlinkRelation' => array('Display', 'Item', 'Dublin Core', 'Relation'),
+    );
+
+    public function hotlinkIdentifier($text, $args) {
+        return $this->_hotlinkField($text, $args);
+    }
+
+    public function hotlinkRelation($text, $args) {
+        return $this->_hotlinkField($text, $args);
+    }
+
+    public function _hotlinkField($text, $args) {
+        if (preg_replace('/[<>]/', '_', filter_var($text, FILTER_VALIDATE_URL)) === $text) {
+            return "<a href=\"" . $text . "\">$text</a>";
+        }
+        else {
+            return $text;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+An Omeka plugin that hotlinks URLs in the Identifier and Relation fields.
